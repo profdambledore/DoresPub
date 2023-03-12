88ba6c8237e92af8901c743ea100b91e41a511d8
profdambledore <dangeorgemarkdore@gmail.com>
2023-03-12 16:25:24 +0000

    Init UE4 Files

---
+++ Source/DoresPub/DoresPubGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DoresPubGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DORESPUB_API ADoresPubGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
