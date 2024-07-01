ce256785ead681fa16998f6459d51720b25ae4e6
profdambledore <dangeorgemarkdore@gmail.com>
2024-07-01 10:50:45 +0100

    v0.0 - Unreal Upload

---
+++ Source/DoresPub/DoresPub.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "DoresPub.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DoresPub, "DoresPub" );
